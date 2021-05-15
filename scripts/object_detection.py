892da7f2c3fbe419a8189a948eb8b626fb1fb0bb
namikxgithub <namanmalik0210@gmail.com>
2021-05-15 23:42:41 +0530

    object_detection_completed

--- scripts/object_detection.py
+++ scripts/object_detection.py
@@ -13,14 +13,22 @@ class Object_tracker:
     def __init__(self):
         rospy.Subscriber("/object_pose", object_pose, self.object_pose_update)
         self.pose_pub = rospy.Publisher('/object_pose', object_pose, queue_size=10)
-        rospy.Subscriber("/camera/image_raw", Image, self.image_callback)
+        rospy.Subscriber("/usb_cam/image_raw", Image, self.image_callback)
         self.bridge = CvBridge()
-        self.yellowLower =(30, 100, 50)
-        self.yellowUpper = (60, 255, 255)
-        self.distance = 0
+        self.ob_pose = object_pose()
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
+        self.yellowLower =(110,100,100)
+        self.yellowUpper = (130,255,255)
+        self.reset_ob_val()
+
+    def reset_ob_val(self):
+        self.ob_pose.x = -1
+        self.ob_pose.y = -1
+        self.ob_pose.z = -1
+        self.ob_pose.r = -1
 
     def object_pose_update(self, msg):
-        self.distance = msg.x
+        self.ob_pose.z = msg.x
 
     def image_callback(self, ros_image):
         try:
@@ -37,9 +45,12 @@ class Object_tracker:
 
     def getContours(self, binary_image):
         contours, hierarchy = cv2.findContours(binary_image.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        return contours
+        if contours:
+            return contours
+        else:
+            return 0
 
-    def get_contour_center(contour):
+    def get_contour_center(self, contour):
         M = cv2.moments(contour)
         cx=-1
         cy=-1
@@ -49,19 +60,32 @@ class Object_tracker:
         return cx, cy
 
     def draw_ball_contour(self, binary_image, rgb_image, contours):
+        #print(contours)
         black_image = np.zeros([binary_image.shape[0], binary_image.shape[1],3],'uint8')
         
-        for c in contours:
-            area = cv2.countorArea(c)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            if (area>3000):
-                cv2.drawContours(rgb_image, [c], -1, (150,250,150), 1)
-                cx, cy = self.get_contour_center(c)
-                cv2.circle(rgb_image, (cx,cy),(int)(radius),(0,0,255),1)
-                cv2.circle(black_image, (cx,cy),(int)(radius),(0,0,255),1)
-                
+        if contours == 0:
+            cv2.putText(black_image,'object not found',(20,15), self.font, 0.81,(255,255,255),1)
+
+        else:
+            for c in contours:
+                area = cv2.contourArea(c)
+
+                if (area>500):
+                    ((x, y), radius) = cv2.minEnclosingCircle(c)
+                    cv2.drawContours(rgb_image, [c], -1, (150,250,150), 1)
+                    self.ob_pose.x, self.ob_pose.y = self.get_contour_center(c)
+                    self.ob_pose.r = radius
+                    self.pose_pub.publish(self.ob_pose)
+                    cv2.circle(rgb_image, (int(x),int(y)),int(radius),(0,0,255),1)
+                    cv2.circle(black_image, (self.ob_pose.x,self.ob_pose.y),(int)(radius),(0,0,255),1)
+
+            cv2.putText(black_image,'ob_x = {}'.format(self.ob_pose.x),(15,20), self.font, 0.7,(255,255,255),1)
+            cv2.putText(black_image,'ob_y = {}'.format(self.ob_pose.y),(15,60), self.font, 0.7,(255,255,255),1)
+            cv2.putText(black_image,'ob_z = {}'.format(self.ob_pose.z),(15,100), self.font, 0.7,(255,255,255),1)
+            cv2.putText(black_image,'ob_r = {}'.format(int(self.ob_pose.r)),(15,140), self.font, 0.7,(255,255,255),1)
         cv2.imshow("Object_detector",rgb_image)
         cv2.imshow("Black_image",black_image)
+        cv2.waitKey(1)
 
     def ball_detection(self, image):
         rgb_image = image
@@ -80,6 +104,7 @@ def main():
     except KeyboardInterrupt:
         rospy.loginfo('Shutting down')
     cv2.waitKey(0)
+    del(object)
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
--- scripts/object_tracker.py
+++ scripts/object_tracker.py
@@ -9,8 +9,9 @@ from tortoisebot_object_follower.msg import object_pose
 velocity_msg = Twist()
 ob_pose = object_pose()
 
-vel_pub = None
-pose_pub = None
+# initialize publisher to publish to /cmd_vel topic
+vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+pose_pub = rospy.Publisher('/object_pose', object_pose, queue_size=10)
 
 
 def move(linear,angular):
@@ -22,34 +23,34 @@ def move(linear,angular):
 
 def laser_callback(msg):
     global ob_pose,pose_pub
-    ob_pose.z = msg.ranges[288:431]
+    ob_pose.z = min(min(msg.ranges[288:431]), 10)
     pose_pub.publish(ob_pose)
 
 
 def object_pose_update(msg):
     global ob_pose
-    if msg:
-        ob_pose.x = msg.x
-        ob_pose.y = msg.y
-
+    
+    ob_pose.x = msg.x
+    ob_pose.y = msg.y
 
 def main():
-    global ob_pose, vel_pub, pose_pub
+    global ob_pose
     
     rospy.init_node('ebot_controller')
 
-    # initialize publisher to publish to /cmd_vel topic
-    vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     # subscribe to /odom and /ebot/laser/scan
     rospy.Subscriber('/scan', LaserScan, laser_callback)
     rospy.Subscriber("/object_pose", object_pose, object_pose_update)
-    pose_pub = rospy.Publisher('/object_pose', object_pose, queue_size=10)
 
     rate = rospy.Rate(10)
     move(0,0)
 
     while not rospy.is_shutdown():
-        move(0,0)
+        if ob_pose.x == -1:
+            print('object_not_found')
+            move(0,0)
+        else:
+
         rate.sleep()
 
 
